bc3e653329dcd2ac54da7fa3566eb8aca121bc14
Bruce Z <121924776+bruce414@users.noreply.github.com>
2024-06-30 22:08:08 +1200

    Create main.dart
    This is the root of my application. SIUUUUU

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:lets_eat/view/loginpage.dart';
+import 'view/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LetsEat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LogInPage(),
+    );
+  }
+}
+
